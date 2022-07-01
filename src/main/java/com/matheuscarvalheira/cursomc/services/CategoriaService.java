0d6183bcb7bf08da6225f95d81a925340e472769
matheus carvalheira <matheuscarvalheiraf@gmail.com>
2022-07-01 12:01:08 -0300

    endpoint adjustments /categorias/{id}

--- src/main/java/com/matheuscarvalheira/cursomc/services/CategoriaService.java
+++ src/main/java/com/matheuscarvalheira/cursomc/services/CategoriaService.java
@@ -2,25 +2,23 @@ package com.matheuscarvalheira.cursomc.services;
 
 import java.util.Optional;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.matheuscarvalheira.cursomc.domain.Categoria;
 import com.matheuscarvalheira.cursomc.repositories.CategoriaRepository;
+import com.matheuscarvalheira.cursomc.services.exceptions.ObjectNotFountException;
 
 @Service
 public class CategoriaService {
 
 	@Autowired
 	private CategoriaRepository repo;
-	
 
 	public Categoria find(Integer id) {
-		
 		Optional<Categoria> obj = repo.findById(id);
-		
-		return obj.orElse(null);
-		
+		return obj.orElseThrow(() -> new ObjectNotFountException("Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
 
 }
