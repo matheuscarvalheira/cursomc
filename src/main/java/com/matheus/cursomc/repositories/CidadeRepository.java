6f3fa9f5a1be88c7659863a30742632a2c51e8b2
Matheus Carvalheira <matheuscarvalheiraf@gmail.com>
2023-02-01 21:58:32 -0300

    state and city repositories and relationships

---
+++ src/main/java/com/matheus/cursomc/repositories/CidadeRepository.java
@@ -0,0 +1,12 @@
+package com.matheus.cursomc.repositories;
+
+
+import com.matheus.cursomc.domain.Cidade;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CidadeRepository extends JpaRepository<Cidade, Integer> {
+
+
+}
---
+++ src/main/java/com/matheus/cursomc/repositories/EstadoRepository.java
@@ -0,0 +1,12 @@
+package com.matheus.cursomc.repositories;
+
+
+import com.matheus.cursomc.domain.Estado;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EstadoRepository extends JpaRepository<Estado, Integer> {
+
+
+}
