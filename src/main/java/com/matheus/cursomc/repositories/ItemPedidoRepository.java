8d60b7883221ca37062528c274b693962c1b5483
Matheus Carvalheira <matheuscarvalheiraf@gmail.com>
2023-02-15 16:22:38 -0300

    Repository for order item created

---
+++ src/main/java/com/matheus/cursomc/repositories/ItemPedidoRepository.java
@@ -0,0 +1,12 @@
+package com.matheus.cursomc.repositories;
+
+
+import com.matheus.cursomc.domain.ItemPedido;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ItemPedidoRepository extends JpaRepository<ItemPedido, Integer> {
+
+
+}
