736e885737a615d8c628dc4850b9560197fe3d33
Matheus Carvalheira <matheuscarvalheiraf@gmail.com>
2023-02-23 17:24:29 -0300

    Custom error for deletion created

--- src/main/java/com/matheus/cursomc/resources/exception/ResourceExceptionHandler.java
+++ src/main/java/com/matheus/cursomc/resources/exception/ResourceExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.matheus.cursomc.resources.exception;
 
+import com.matheus.cursomc.services.exceptions.DataIntegrityException;
 import com.matheus.cursomc.services.exceptions.ObjectNotFoundtException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,6 +15,12 @@ public class ResourceExceptionHandler {
     public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundtException e, HttpServletRequest request) {
         StandardError err = new StandardError(HttpStatus.NOT_FOUND.value(), e.getMessage(), System.currentTimeMillis());
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
+    }
+
+    @ExceptionHandler(DataIntegrityException.class)
+    public ResponseEntity<StandardError> dataIntegrity(DataIntegrityException e, HttpServletRequest request) {
+        StandardError err = new StandardError(HttpStatus.BAD_REQUEST.value(), e.getMessage(), System.currentTimeMillis());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
     }
 
 }
---
+++ src/main/java/com/matheus/cursomc/services/exceptions/DataIntegrityException.java
@@ -0,0 +1,13 @@
+package com.matheus.cursomc.services.exceptions;
+
+public class DataIntegrityException extends RuntimeException {
+
+    public DataIntegrityException(String msg){
+        super(msg);
+    }
+
+    public DataIntegrityException(String msg, Throwable cause){
+        super(msg,cause);
+    }
+
+}
