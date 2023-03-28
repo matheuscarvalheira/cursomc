06686c8114af3125e1a1573aa76a51666724f83d
Matheus Carvalheira <matheuscarvalheiraf@gmail.com>
2023-03-28 09:44:30 -0300

    fixing variable errors

--- src/main/java/com/matheus/cursomc/resources/exception/ValidationError.java
+++ src/main/java/com/matheus/cursomc/resources/exception/ValidationError.java
@@ -11,7 +11,7 @@ public class ValidationError extends StandardError {
         super(status, msg, timeStamp);
     }
 
-    public List<FieldMessage> getErros() {
+    public List<FieldMessage> getErrors() {
         return errors;
     }
 
