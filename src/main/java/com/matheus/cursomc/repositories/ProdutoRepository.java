12cb4b86e02efa592d564a9b783f691142ba0a81
Matheus Carvalheira <matheuscarvalheiraf@gmail.com>
2023-04-14 18:31:34 -0300

    searching products by name

--- src/main/java/com/matheus/cursomc/repositories/ProdutoRepository.java
+++ src/main/java/com/matheus/cursomc/repositories/ProdutoRepository.java
@@ -6,13 +6,18 @@ import com.matheus.cursomc.domain.Produto;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Repository
 public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
 
-    Page<Produto> search(String nome, List<Categoria> categorias, Pageable pageRequest);
+    @Transactional(readOnly = true)
+    @Query("SELECT DISTINCT obj FROM Produto obj INNER JOIN obj.categorias cat WHERE obj.nome LIKE %:nome% AND cat IN :categorias")
+    Page<Produto> search(@Param("nome") String nome, @Param("categorias") List<Categoria> categorias, Pageable pageRequest);
 
 }
