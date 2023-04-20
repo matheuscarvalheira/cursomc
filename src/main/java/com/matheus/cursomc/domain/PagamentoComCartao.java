4dd8cbb469212b0c4dd91bd19a1b0abba3a7a534
Matheus Carvalheira <matheuscarvalheiraf@gmail.com>
2023-04-19 22:33:13 -0300

    configuring subclasses and the abstract class

--- src/main/java/com/matheus/cursomc/domain/PagamentoComCartao.java
+++ src/main/java/com/matheus/cursomc/domain/PagamentoComCartao.java
@@ -1,10 +1,12 @@
 package com.matheus.cursomc.domain;
 
+import com.fasterxml.jackson.annotation.JsonTypeName;
 import com.matheus.cursomc.domain.enums.EstadoPagamento;
 
 import javax.persistence.Entity;
 
 @Entity
+@JsonTypeName("pagamentoComCartao")
 public class PagamentoComCartao extends Pagamento{
 
     private Integer numeroDeParcelas;
