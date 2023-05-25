dc128e59506d7c136d1b7bb407b20083639faca9
Matheus Carvalheira <matheuscarvalheiraf@gmail.com>
2023-05-25 14:34:53 -0300

    creating test profile

---
+++ src/main/java/com/matheus/cursomc/config/TestConfig.java
@@ -0,0 +1,23 @@
+package com.matheus.cursomc.config;
+
+import com.matheus.cursomc.services.DBService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import java.text.ParseException;
+
+@Configuration
+@Profile("test")
+public class TestConfig {
+
+    @Autowired
+    private DBService dbService;
+
+    @Bean
+    public boolean instantiateDatabase() throws ParseException {
+        dbService.instantiateTestDatabase();
+        return true;
+    }
+}
