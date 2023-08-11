1458794f7f502e5363fc531f9b4c4493d453709f
Matheus Carvalheira <matheuscarvalheiraf@gmail.com>
2023-08-11 18:05:15 -0300

    Implementing SmtpEmailService with Google Server

--- src/main/java/com/matheus/cursomc/config/DevConfig.java
+++ src/main/java/com/matheus/cursomc/config/DevConfig.java
@@ -1,6 +1,8 @@
 package com.matheus.cursomc.config;
 
 import com.matheus.cursomc.services.DBService;
+import com.matheus.cursomc.services.EmailService;
+import com.matheus.cursomc.services.SmtpEmailService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -28,5 +30,11 @@ public class DevConfig {
 
         dbService.instantiateTestDatabase();
         return true;
+    }
+
+
+    @Bean
+    public EmailService emailService() {
+        return new SmtpEmailService();
     }
 }
